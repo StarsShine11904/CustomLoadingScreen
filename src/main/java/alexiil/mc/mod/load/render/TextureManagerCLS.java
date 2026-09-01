70cae13169c290178ba2ffbbace231fc72e525a3
StarsShine11904 <89504980+StarsShine11904@users.noreply.github.com>
2026-09-01 16:37:24 +0800

    Refactor TextureManagerCLS to use Identifier

--- src/main/java/alexiil/mc/mod/load/render/TextureManagerCLS.java
+++ src/main/java/alexiil/mc/mod/load/render/TextureManagerCLS.java
@@ -5,11 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import net.minecraft.client.renderer.texture.ITextureObject;
-import net.minecraft.client.renderer.texture.ITickableTextureObject;
-import net.minecraft.client.renderer.texture.TextureManager;
-import net.minecraft.client.resources.IResourceManager;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.client.texture.AbstractTexture;
+import net.minecraft.client.texture.TextureManager;
+import net.minecraft.client.texture.TextureTickListener;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
 
 import alexiil.mc.mod.load.CLSLog;
 import alexiil.mc.mod.load.CustomLoadingScreen;
@@ -17,27 +17,26 @@ import alexiil.mc.mod.load.CustomLoadingScreen;
 public class TextureManagerCLS extends TextureManager {
 
     /** Map of texture to last access tick. */
-    private final Map<ResourceLocation, Long> textures = new HashMap<>();
-    // If we're forced to put a full object into the map value, we may as well intern them by only creating them once
+    private final Map<Identifier, Long> textures = new HashMap<>();
     private Long currentTime = System.currentTimeMillis();
 
-    public TextureManagerCLS(IResourceManager resourceManager) {
+    public TextureManagerCLS(ResourceManager resourceManager) {
         super(resourceManager);
     }
 
-    private void onTextureAccess(ResourceLocation resource) {
+    private void onTextureAccess(Identifier resource) {
         textures.put(resource, currentTime);
     }
 
     @Override
-    public void bindTexture(ResourceLocation resource) {
+    public void bindTexture(Identifier resource) {
         super.bindTexture(resource);
         onTextureAccess(resource);
     }
 
     @Override
-    public ITextureObject getTexture(ResourceLocation resource) {
-        ITextureObject obj = super.getTexture(resource);
+    public AbstractTexture getTexture(Identifier resource) {
+        AbstractTexture obj = super.getTexture(resource);
         if (obj != null) {
             onTextureAccess(resource);
         }
@@ -45,26 +44,20 @@ public class TextureManagerCLS extends TextureManager {
     }
 
     @Override
-    public void deleteTexture(ResourceLocation textureLocation) {
-        super.deleteTexture(textureLocation);
+    public void destroyTexture(Identifier textureLocation) {
+        super.destroyTexture(textureLocation);
         textures.remove(textureLocation);
     }
 
     @Override
-    public boolean loadTexture(ResourceLocation textureLocation, ITextureObject textureObj) {
+    public boolean registerTexture(Identifier textureLocation, AbstractTexture textureObj) {
         onTextureAccess(textureLocation);
-        return super.loadTexture(textureLocation, textureObj);
-    }
-
-    @Override
-    public boolean loadTickableTexture(ResourceLocation textureLocation, ITickableTextureObject textureObj) {
-        onTextureAccess(textureLocation);
-        return super.loadTickableTexture(textureLocation, textureObj);
+        return super.registerTexture(textureLocation, textureObj);
     }
 
     public void deleteAll() {
-        for (ResourceLocation location : textures.keySet().toArray(new ResourceLocation[0])) {
-            deleteTexture(location);
+        for (Identifier location : textures.keySet().toArray(new Identifier[0])) {
+            destroyTexture(location);
         }
     }
 
@@ -82,21 +75,21 @@ public class TextureManagerCLS extends TextureManager {
 
         currentTime = next;
 
-        long minTime = currentTime - (CustomLoadingScreen.textureClearInterval * 1000);
+        long minTime = currentTime - (CustomLoadingScreen.textureClearInterval * 1000L);
 
-        List<ResourceLocation> toRemove = new ArrayList<>();
+        List<Identifier> toRemove = new ArrayList<>();
 
-        for (Map.Entry<ResourceLocation, Long> entry : textures.entrySet()) {
+        for (Map.Entry<Identifier, Long> entry : textures.entrySet()) {
             if (entry.getValue() < minTime) {
                 toRemove.add(entry.getKey());
             }
         }
 
-        for (ResourceLocation tex : toRemove) {
+        for (Identifier tex : toRemove) {
             if (CustomLoadingScreen.debugResourceLoading) {
                 CLSLog.info("[debug] Automatically deleting texture " + tex);
             }
-            deleteTexture(tex);
+            destroyTexture(tex);
         }
     }
 }
